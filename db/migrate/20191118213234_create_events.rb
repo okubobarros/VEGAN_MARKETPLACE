74aa001f0322abf0e09bbead0c9e8d376386e804
Camilla Kunii <milla.mk@hotmail.com>
2019-11-18 18:37:00 -0300

    created model & controller of Event

---
+++ db/migrate/20191118213234_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.text :description
+      t.integer :limit_of_guest
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :address
+      t.string :type_of_meal
+      t.string :calendar
+      t.string :type_of_event
+      t.string :time
+
+      t.timestamps
+    end
+  end
+end
