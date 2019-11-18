71408d27988f6d927b3a668569b8aa7fa1f3e55f
Camilla Kunii <milla.mk@hotmail.com>
2019-11-18 17:47:50 -0300

    finished user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_183035) do
+ActiveRecord::Schema.define(version: 2019_11_18_194804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_183035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.boolean "is_chef"
+    t.boolean "is_client"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
