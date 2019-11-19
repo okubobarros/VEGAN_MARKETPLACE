61dd88f3c5b0c0d7ce0e969f63459024bc02125c
Cesar Corregiari <cesarcorregiari@gmail.com>
2019-11-19 17:39:29 -0300

    finished the events index method with dynamic search and improved homepage front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Event.destroy_all
+
+user = User.create!(first_name: "Cesar", last_name: "User", email: "teste@teste.com", password: "123456",)
+
+Event.create!(user: user, description: "Enjoy an Arabian dinner while in a magic carpet", title: "Arabian Nights",
+	address: "Arabia", type_of_meal: "Arabian", type_of_event: "dinner")
+Event.create!(user: user, description: "Burritos and Tacos at sunrise", title: "Mexican Breakfast",
+	address: "Mexico", type_of_meal: "Mexican", type_of_event: "breakfast")
+Event.create!(user: user, description: "Celebrate Ravioli day", title: "Italian Party",
+	address: "Italia", type_of_meal: "Italian", type_of_event: "lunch")
+
+
+
