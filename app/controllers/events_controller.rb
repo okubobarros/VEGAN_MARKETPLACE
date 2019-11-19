bfb22a60c327c38917371f2d1a8c1abd92f5f2e1
Gustavo <guscooke14@gmail.com>
2019-11-19 11:40:44 -0300

    view event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,19 @@
 class EventsController < ApplicationController
+  def edit
+    @event = edit.find(params[:id])
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+end
+
+private
+
+def event_params
+  params.require(:event).permit(:title, :address, :description, :type_of_meal, :calendar, :type_of_event, :time, :limit_of_guests)
 end
