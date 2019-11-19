3c196aac74d482c0d165534c0929f6cbf58b312f
Cesar Corregiari <cesarcorregiari@gmail.com>
2019-11-19 13:43:32 -0300

    working on delete method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,5 @@
 class EventsController < ApplicationController
 
-
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to root_path
   def new
     @event = Event.new
   end
@@ -18,16 +13,17 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to event_path
+  end
+
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :address, :type_of_meal,
-                                  :calendar, :type_of_event, :time, :limit_of_guests)
-
+    params.require(:event).permit(:title, :description, :address, :type_of_meal, :calendar, :type_of_event, :time, :limit_of_guests)
   end
 end
 
 
-# <%= link_to "Delete", event_path(event),
-      # method: :delete,
-      # data: { confirm: "Are you sure?" } %>
