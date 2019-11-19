1938233e731612176a8c09a40a3c7b8a6c930e1a
Cesar Corregiari <cesarcorregiari@gmail.com>
2019-11-19 11:37:21 -0300

    working on destroy event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,14 @@
 class EventsController < ApplicationController
+
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path
+  end
 end
+
+
+# <%= link_to "Delete", event_path(event),
+      # method: :delete,
+      # data: { confirm: "Are you sure?" } %>
