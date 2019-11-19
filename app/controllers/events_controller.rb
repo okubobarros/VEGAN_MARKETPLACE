16e7a3c85063d6946d63aeb0c306146937200cf3
Camilla Kunii <milla.mk@hotmail.com>
2019-11-19 13:08:36 -0300

    adjusts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,17 @@
 class EventsController < ApplicationController
 
   def edit
-    @event = edit.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
     if @event.update(event_params)
       redirect_to event_path(@event)
     else
       render :edit
     end
   end
-
-
 
   def new
     @event = Event.new
@@ -27,11 +26,14 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:title, :description, :address, :type_of_meal,
                                   :calendar, :type_of_event, :time, :limit_of_guests)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :events
+  has_many :events, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
