e4648210f06b3bd6726e6392cb4b094e1d2890c0
Camilla Kunii <milla.mk@hotmail.com>
2019-11-21 17:30:37 -0300

    Davis me ajudou com o efeito do botão

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,8 +5,6 @@ class EventsController < ApplicationController
     if params[:query].present?
        sql_query = "title ILIKE :query OR address ILIKE :query OR type_of_meal ILIKE :query OR type_of_event ILIKE :query"
        @events = Event.where(sql_query, query: "%#{params[:query]}%").page(params[:page]).per(10)
-      sql_query = "title ILIKE :query OR address ILIKE :query OR type_of_meal ILIKE :query OR type_of_event ILIKE :query"
-      @events = Event.where(sql_query, query: "%#{params[:query]}%")
     else
       @events = Event.all.page(params[:page]).per(3)
     end
